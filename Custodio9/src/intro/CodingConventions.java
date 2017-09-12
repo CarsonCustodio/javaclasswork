dec18f23843cf2573469733dfe4e4ff75b8ea16d
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-09-12 14:50:38 -0400

    added coding conventions

---
+++ Custodio9/src/intro/CodingConventions.java
@@ -0,0 +1,20 @@
+package intro;
+
+public class CodingConventions {
+	
+	
+	/*
+	 * this is a constructor, it is like a method except the "return type" is an instance of the class
+	 * for this reason, a constructor must always be named after the class
+	 */
+	public CodingConventions() {
+		
+	}
+	
+	//normal method "void"
+	public void doStuff() {
+		//static method call:
+		
+		System.out.println("hello world");
+	}
+}
--- Custodio9/src/intro/IntroMain.java
+++ Custodio9/src/intro/IntroMain.java
@@ -1,6 +1,23 @@
-package intro;
+package intro; //package declaration matches the folder where this folder is located
 
-public class IntroMain {
-// x
-  // more
+//class header
+public class IntroMain {	
+	
+	//this is the first method that is executed. It is static because it is independent of instances.
+	public static void main(String[] args) {
+		//this is the declaration of an instance of CodingConventions
+		//in the same line of code, it is also being instantiated
+		//note the use of the word "new" this word is always used when calling constructors
+		CodingConventions conventionsInstance = new CodingConventions();
+		//local variables are not fields (has-a relationships)
+		//they are variables that are only used in the scope of a method
+		//after the method, they are destroyed
+		
+		/*
+		 * instance method call (the method belongs to the instance, not the class)
+		 */
+		conventionsInstance.doStuff();
+	}
+	
+	
 }
