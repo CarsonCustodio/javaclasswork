6f19888eb3e909a21d11f68cabdd8159184a3944
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-09-12 15:18:00 -0400

    a

--- Custodio9/src/intro/CodingConventions.java
+++ Custodio9/src/intro/CodingConventions.java
@@ -2,19 +2,29 @@ package intro;
 
 public class CodingConventions {
 	
+	//FIELDS ARE AT THE TOP
+	private String name; //Declare fields only, instantiate in the constructor
+	private String description;
+	
 	
 	/*
 	 * this is a constructor, it is like a method except the "return type" is an instance of the class
 	 * for this reason, a constructor must always be named after the class
 	 */
-	public CodingConventions() {
+	public CodingConventions(String name, int descriptionIndex) {
+		//instantiate variables now:
 		
+		//there are 2 variables called "name" the local variable and the field
+		//distinguish between the 2 by using the word "this" (field)
+		this.name = name;
+		//this is a static call to a constant
+		description = IntroMain.DESCRIPTIONS[descriptionIndex];
 	}
 	
 	//normal method "void"
 	public void doStuff() {
 		//static method call:
 		
-		System.out.println("hello world");
+		System.out.println(name + description);
 	}
 }
--- Custodio9/src/intro/IntroMain.java
+++ Custodio9/src/intro/IntroMain.java
@@ -3,20 +3,29 @@ package intro; //package declaration matches the folder where this folder is loc
 //class header
 public class IntroMain {	
 	
+	public static final String[] DESCRIPTIONS = {" is a teacher at BTHS", " is a student at BTHS"};
+	
 	//this is the first method that is executed. It is static because it is independent of instances.
 	public static void main(String[] args) {
 		//this is the declaration of an instance of CodingConventions
 		//in the same line of code, it is also being instantiated
 		//note the use of the word "new" this word is always used when calling constructors
-		CodingConventions conventionsInstance = new CodingConventions();
-		//local variables are not fields (has-a relationships)
-		//they are variables that are only used in the scope of a method
-		//after the method, they are destroyed
 		
-		/*
-		 * instance method call (the method belongs to the instance, not the class)
-		 */
-		conventionsInstance.doStuff();
+		int numberOfPeople = 10;
+		for(int i = 0; i < numberOfPeople; i++) {
+			CodingConventions conventionsInstance = new CodingConventions("Carson", i%2);
+			
+			//local variables are not fields (has-a relationships)
+			//they are variables that are only used in the scope of a method
+			//after the method, they are destroyed
+			
+			/*
+			 * instance method call (the method belongs to the instance, not the class)
+			 */
+			conventionsInstance.doStuff();
+		}
+		
+		
 	}
 	
 	
