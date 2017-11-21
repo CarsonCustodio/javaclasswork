1c3cf431082aac2db3414c12a2f47c022859d9b1
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-21 10:30:51 -0500

    a

--- Custodio9/src/javaTheHardWay/Predator.java
+++ Custodio9/src/javaTheHardWay/Predator.java
@@ -1,5 +1,41 @@
 package javaTheHardWay;
 
-public class Predator {
-
+public class Predator extends ReproductionAnimal {
+	
+	public Predator(Habitat matingArea) {
+		super(matingArea);
+		setMaxLitterSize(3);
+	}
+	
+	public Predator(Habitat matingArea, String description, Trait trait1, Trait trait2) {
+		super(matingArea, description, trait1, trait2);
+		setMaxLitterSize(3);
+	}
+	
+	public static final String[] PREDATORS = {"wolf", "bear", "lion", "bobcat"};
+			
+	public static void main(String[] args) {
+		Wilderness wilderness = new Wilderness("A wooded area", 10);
+		Predator p = new Predator(wilderness);
+		wilderness.addAnimal(p);
+		wilderness.simulate(2);
+	}
+	
+	public ReproductionAnimal getOffspring(ReproductionAnimal mate) {
+		return new Predator(habitat, getDescription(), Trait.getDominateTrait(getTrait1(), mate.getTrait2()), Trait.getDominateTrait(getTrait1(), mate.getTrait2()));
+	}
+	
+	public boolean canEat() {
+		int attempts = 0;
+		while(attempts < 3) {
+			int tIndex = (int)(Math.random() * habitat.getAnimals().length);
+			Animal target = habitat.getAnimals()[tIndex];
+			if(target instanceof Prey) {
+				habitat.removeAnimal(tIndex);
+				return true;
+			}
+			attempts++;
+		}
+		return false;
+	}
 }
